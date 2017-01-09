eef664bb6cad2824fec4eb0c2cd7165134d73739
Graham Blevins <hgblevins@gmail.com>
2017-01-09 18:54:39 -0500

    cacheable accessors, cleanup, version bump

--- Base.php
+++ Base.php
@@ -17,70 +17,6 @@ abstract class Base extends Model
         parent::__construct($this->user->to_array());
     }
 
-    public function addMeta($key, $value, $unique = false)
-    {
-        return add_user_meta($this->ID, $key, $value, $unique);
-    }
-
-    public function addRole($role)
-    {
-        $this->user->add_role($role);
-    }
-
-    public function deleteMeta($key, $value = '')
-    {
-        return delete_user_meta($this->ID, $key, $value);
-    }
-
-    public function description()
-    {
-        return $this->user->description;
-    }
-
-    public function firstName()
-    {
-        return $this->user->first_name;
-    }
-
-    public function fullName()
-    {
-        $fullName = trim(implode([$this->user->first_name, $this->user->last_name], ' '));
-
-        if (!empty($fullName)) {
-            return $this->full_name = $fullName;
-        }
-    }
-
-    public function getIdAttribute($value)
-    {
-        return (int) $value;
-    }
-
-    public function getMeta($key, $single = false)
-    {
-        return get_user_meta($this->ID, $key, $single);
-    }
-
-    public function getRoles()
-    {
-        return $this->user->roles;
-    }
-
-    public function hasPosts()
-    {
-        return $this->posts->count() > 0;
-    }
-
-    public function hasRole($role)
-    {
-        return in_array($role, $this->user->roles);
-    }
-
-    public function lastName()
-    {
-        return $this->user->last_name;
-    }
-
     public function newQuery()
     {
         $query = (new Builder(new Query(new WP_User_Query)))->setModel($this);
@@ -88,14 +24,91 @@ abstract class Base extends Model
         return $query;
     }
 
-    public function posts()
+    public function getCacheableAccessors()
     {
-        return $this->posts = Post::query()->author($this->ID)->get();
+        return array_merge(parent::getCacheableAccessors(), [
+            'posts',
+            'posts_url',
+        ]);
     }
 
-    public function postsUrl()
+    public function getDescriptionAttribute()
     {
-        return $this->posts_url = get_author_posts_url($this->ID);
+        return $this->user->description;
+    }
+
+    public function getFirstNameAttribute()
+    {
+        return $this->user->first_name;
+    }
+
+    public function getFullNameAttribute()
+    {
+        $fullName = trim(implode([$this->user->first_name, $this->user->last_name], ' '));
+
+        if (!empty($fullName)) {
+            return $fullName;
+        }
+    }
+
+    public function getIdAttribute($value)
+    {
+        return (int)$value;
+    }
+
+    public function getLastNameAttribute()
+    {
+        return $this->user->last_name;
+    }
+
+    public function getPostsAttribute()
+    {
+        return Post::query()->author($this->ID)->get();
+    }
+
+    public function getPostsUrlAttribute()
+    {
+        return get_author_posts_url($this->ID);
+    }
+
+    public function hasPosts()
+    {
+        return $this->posts->count() > 0;
+    }
+
+    public function getMeta($key, $single = false)
+    {
+        return get_user_meta($this->ID, $key, $single);
+    }
+
+    public function addMeta($key, $value, $unique = false)
+    {
+        return add_user_meta($this->ID, $key, $value, $unique);
+    }
+
+    public function updateMeta($key, $value, $previousValue = '')
+    {
+        return update_user_meta($this->ID, $key, $value, $previousValue);
+    }
+
+    public function deleteMeta($key, $value = '')
+    {
+        return delete_user_meta($this->ID, $key, $value);
+    }
+
+    public function hasRole($role)
+    {
+        return in_array($role, $this->user->roles);
+    }
+
+    public function getRoles()
+    {
+        return $this->user->roles;
+    }
+
+    public function addRole($role)
+    {
+        $this->user->add_role($role);
     }
 
     public function removeRole($role)
@@ -115,11 +128,6 @@ abstract class Base extends Model
         foreach ($roles as $role) {
             $this->addRole($role);
         }
-    }
-
-    public function updateMeta($key, $value, $previousValue = '')
-    {
-        return update_user_meta($this->ID, $key, $value, $previousValue);
     }
 
     public static function doRefreshRewrites($id = 0, $exclude = false)
